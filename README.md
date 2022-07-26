b4c82a3c7f2a794dfcab9e085cacfe7a47bac438
Klaus Cedillo <klausmcedillo@gmail.com>
2022-07-25 20:39:39 -0500

    README file Creation Complete

---
+++ README.md
@@ -0,0 +1,67 @@
+# asd
+
+___
+
+### Description
+
+asd
+
+---
+
+### Table of Contents
+
+
+
+
+
+Installation
+
+Usage
+
+License
+
+Contributing
+
+Tests
+
+Questions
+
+---
+
+### Installation
+
+asd
+
+---
+
+### Usage Information
+
+asd
+
+---
+
+### License
+
+Public Domain
+
+---
+
+### Contributing
+
+asd
+
+---
+
+### Tests
+
+adsasdasd
+
+---
+
+### Questions
+
+asd
+
+asd
+
+---
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+
 
 inquirer
     .prompt([
@@ -50,8 +52,47 @@ inquirer
         },
     ])
     .then((response) => {
-        let answers = response;
-        console.log(answers);
+
+
+        fs.writeFile('README.md', 
+`# ${response.title}\n
+___\n
+### Description\n
+${response.description}\n
+---\n
+### Table of Contents\n
+\n
+\n
+Installation\n
+Usage\n
+License\n
+Contributing\n
+Tests\n
+Questions\n
+---\n
+### Installation\n
+${response.installation}\n
+---\n
+### Usage Information\n
+${response.usage}\n
+---\n
+### License\n
+${response.license}\n
+---\n
+### Contributing\n
+${response.contributing}\n
+---\n
+### Tests\n
+${response.tests}\n
+---\n
+### Questions\n
+${response.github_user}\n
+${response.email}\n
+---
+`,      (err) =>
+            err ? console.error(err) : console.log('Success!')
+        );
+        
 
     });
 
